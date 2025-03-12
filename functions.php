f09906a483ab45d21e8a767199edecbf518aed4d
k.eii <68538183+jonscafe@users.noreply.github.com>
2025-03-12 02:21:25 +0000

    Improve database connection handling in functions.php by changing host to 127.0.0.1, adding charset, and enhancing error messages.

--- functions.php
+++ functions.php
@@ -1,14 +1,16 @@
 <?php
 
 function pdo_connect(){
-    $DATABASE_HOST = 'localhost';
+    $DATABASE_HOST = '127.0.0.1';
     $DATABASE_USER = 'useruas';
     $DATABASE_PASS = 'useruas';
     $DATABASE_NAME = 'uas';
     try {
-    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
+        $pdo = new PDO("mysql:host=$DATABASE_HOST;dbname=$DATABASE_NAME;charset=utf8", $DATABASE_USER, $DATABASE_PASS);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
     } catch (PDOException $exception) {
-    	die ('Failed to connect to database!');
+        die ('Failed to connect to database! ' . $exception->getMessage());
     }
 }
 
